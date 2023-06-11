373bacf7ea4ba06663da1dfdada95d3f640d3d50
Rocio Hachen <rociohachen@hotmail.com>
2023-06-11 13:16:35 -0300

    Modified b1 and completed b2

---
+++ Practica B/b2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Crear un script que reciba exactamente dos argumentos, el primero
+#corresponderá a un número de mes y el segundo a un año.
+#Deberá mostrar por pantalla el calendario del mes y año con el día destacado
+#Tip: analizar el comando cal
+#Nota: evitaremos por ahora la validación de la cantidad
+#y valor de los argumentos enviados
+
+ncal $1 $2
+exit 0
